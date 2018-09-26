2e06276dcec082d5f734534567ce0984f2186cca
Stephan Bösch-Plepelits <skunk@cg.tuwien.ac.at>
2018-09-26 12:58:26 +0200

    NeDB

---
+++ nedb.js
@@ -0,0 +1,58 @@
+const NeDB = require('nedb')
+const fs = require('fs')
+const measureTime = require('measure-time')
+const async = require('async')
+
+let buildings = new NeDB()
+
+let bbox = {
+  minlat: 48.1813,
+  minlon: 16.3201,
+  maxlat: 48.2352,
+  maxlon: 16.4195
+}
+
+let data = JSON.parse(fs.readFileSync('data.json'))
+
+let loadElapsed = measureTime()
+
+async.each(
+  data.elements,
+  (element, done) => {
+    let data = element.bounds
+    data.id = element.id
+
+    buildings.insert(data, done)
+  },
+  cont
+)
+
+let getElapsed
+let count = 0
+
+function cont () {
+  console.log('loading: ', loadElapsed().millisecondsTotal + 'ms')
+
+  getElapsed = measureTime()
+
+  async.times(1000, (n, done) => {
+    let lat = Math.random() * (bbox.maxlat - bbox.minlat) + bbox.minlat
+    let lon = Math.random() * (bbox.maxlon - bbox.minlon) + bbox.minlon
+    let size = Math.random() * 0.2
+
+    buildings.find({
+      minlat: { '$gte': lat },
+      minlon: { '$gte': lon },
+      maxlat: { '$lte': lat + size },
+      maxlon: { '$lte': lon + size }
+    },
+    (err, items) => {
+      count += items.length
+      done()
+    })
+  }, end)
+}
+
+function end () {
+  console.log('avg: ' + count / 1000, getElapsed().millisecondsTotal + 'ms')
+}
