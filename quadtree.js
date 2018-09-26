0db4801ac666aa665ab649866be7345b1b3854d4
Stephan Bösch-Plepelits <skunk@cg.tuwien.ac.at>
2018-09-26 11:52:53 +0200

    Initial commit

---
+++ quadtree.js
@@ -0,0 +1,44 @@
+const Quadtree = require('quadtree-lookup')
+const fs = require('fs')
+const measureTime = require('measure-time')
+
+
+let qt = new Quadtree.Quadtree(new Quadtree.Box(
+  new Quadtree.Point(-90, -180),
+  new Quadtree.Point(90, 180)
+))
+
+let bbox = {
+  minlat: 48.1813,
+  minlon: 16.3201,
+  maxlat: 48.2352,
+  maxlon: 16.4195
+}
+
+let data = JSON.parse(fs.readFileSync('data.json'))
+data.elements.forEach(
+  (element) => {
+    let box = new Quadtree.Box(
+      new Quadtree.Point(element.bounds.minlat, element.bounds.minlon),
+      new Quadtree.Point(element.bounds.maxlat, element.bounds.maxlon)
+    )
+    qt.insert(box, element)
+  }
+)
+
+let getElapsed = measureTime()
+
+for (var i = 0; i < 1000; i++) {
+  let lat = Math.random() * (bbox.maxlat - bbox.minlat) + bbox.minlat
+  let lon = Math.random() * (bbox.maxlon - bbox.minlon) + bbox.minlon
+  let size = Math.random() * 0.2
+
+  let box = new Quadtree.Box(
+    new Quadtree.Point(lat, lon),
+    new Quadtree.Point(lat + size, lon + size) 
+  )
+
+  let items = qt.queryRange(box)
+}
+
+console.log(getElapsed().millisecondsTotal + 'ms')
